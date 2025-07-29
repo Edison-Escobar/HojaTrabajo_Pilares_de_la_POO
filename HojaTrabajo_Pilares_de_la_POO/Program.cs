5af0c431d4c311b5a52a2d38285f599831669c35
Edison-Escobar <adrianesco715@gmail.com>
2025-07-29 11:12:09 -0600

    Update Program.cs

--- HojaTrabajo_Pilares_de_la_POO/Program.cs
+++ HojaTrabajo_Pilares_de_la_POO/Program.cs
@@ -1,4 +1,4 @@
-﻿using HojaTrabajo_Pilares_de_la_POO.Ejercicio1;
+using HojaTrabajo_Pilares_de_la_POO.Ejercicio1;
 using HojaTrabajo_Pilares_de_la_POO.Ejercicio2;
 using HojaTrabajo_Pilares_de_la_POO.Ejercicio3;
 using System;
@@ -6,6 +6,47 @@ using System;
 namespace HojaTrabajo_Pilares_de_la_POO
 {
     class Program
+    {
+        static void Main()
+        {
+            Biblioteca b = new Biblioteca();
+            b.registrar("El Principito", "Saint-Exupéry");
+            b.registrar("1984", "George Orwell");
+            b.MostrarLibrosDisponibles();
+            b.Prestar("El Principito", "Saint-Exupéry");
+            b.MostrarLibrosDisponibles();
+            b.Devolver("El Principito");
+            b.MostrarLibrosDisponibles();
+            b.registrar("Cien años de soledad", "Gabriel García Márquez,");
+            b.registrar("Orgullo y prejuicio", "Jane Auste");
+            b.registrar("Don Quijote de la Mancha", "Miguel de Cervantes");
+            b.MostrarLibrosDisponibles();
+            b.Prestar("Don Quijote de la Mancha", "Miguel de Cervantes");
+            b.MostrarLibrosDisponibles();
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Program1
+    {
+        static void Main()
+        {
+            Usuario u = new Usuario("Carlos");
+
+            u.AsignarDpi("1234567890123");
+            u.IngrsarContrasenia("clave123");
+            u.MostrarNombre();
+
+            Console.WriteLine("Autenticando con clave 'clave123': " + u.Autenticar("clave123"));
+            Console.WriteLine("Autenticando con clave 'otra': " + u.Autenticar("otraa"));
+
+            Console.ReadLine();
+        }
+    }
+
+    class Program3
     {
         static void Main()
         {
@@ -29,6 +70,9 @@ namespace HojaTrabajo_Pilares_de_la_POO
             inventario.MostrarInventario();
 
             inventario.RetirarProducto("Peras", 11);
+            inventario.MostrarInventario();
+
+            inventario.RetirarProducto("Peras", 5);
             inventario.MostrarInventario();
         }
     }
